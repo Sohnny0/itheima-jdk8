f0cc9c2565ee36c7b90bc54d314db0373ed9a6b3
505530098@qq.com <505530098@qq.com>
2020-07-19 23:41:44 +0800

    1

--- src/com/itheima/defaultAndStatic/MySon.java
+++ src/com/itheima/defaultAndStatic/MySon.java
@@ -2,6 +2,16 @@ package com.itheima.defaultAndStatic;
 
 public class MySon extends MyParent implements MyInterface{
 
+   /* 当一个类继承一个类，并且实现一个或者多个接口的时候，其中，父类和父接口中存在相同的方法。
+
+            　　如果子类中存在该方法的实现体或者说是覆盖，则使用该类的对象去掉用该方法时候，其实掉用的是来自接口的实现方法，而不是来自父类方法的覆盖。
+
+            　　如果子类中不存在该方法的实现[或者覆盖],在使用该类对象掉用该方法的时候，就会使用从父类继承的方法。同时将这个从父类继承来的方法当作接口方法的实现，也就可以不再实现接口的方法体。
+
+    另外值得注意的就是，extends & implements 的书写顺序不能改变。
+
+    为了方便记忆可以认为：接口的优先级别要高于父类。
+    */
 
     public static void main(String[] args) {
         MySon mySon=new MySon();
--- src/com/itheima/method/Demo.java
+++ src/com/itheima/method/Demo.java
@@ -4,11 +4,11 @@ public class Demo {
 
     public static void main(String[] args) {
 
-        DemoServiceImpl demoServiceImpl = new DemoServiceImpl();
+     /*   DemoServiceImpl demoServiceImpl = new DemoServiceImpl();
         // 执行默认方法
         System.out.println(demoServiceImpl.defaultMethod());
         // 执行抽象方法
-        System.out.println(DemoService.staticMethod());
+        System.out.println(DemoService.staticMethod());*/
 
       /*  DemoService demoService = new DemoService() {
             @Override
@@ -24,14 +24,16 @@ public class Demo {
 
 
       // java中接口定义是否支持方法体？ 支持, java8之后
-       new DemoService() {
+      DemoService a =  new DemoService() {
            @Override
            public String abstractMethod() {
                return null;
            }
 
-
        };
+        String s = DemoService.staticMethod();
+        System.out.println(s);
+
 
     }
 }
